f2a01b2c706848b0fc063be2e591c00beb324b54
Javier S <javier.sixto@ulex.fr>
2026-02-06 13:29:24 +0100

    Remove trim function warnings as C++11 with updates from https://stackoverflow.com/questions/216823/how-can-i-trim-a-stdstring
    Signed-off-by: Javier S <javier.sixto@ulex.fr>

--- src/Grammar.cpp
+++ src/Grammar.cpp
@@ -1499,14 +1499,14 @@ std::string Grammar::printExpansionType(Expansion * e) {
 // trim from start
 inline std::string &Grammar::ltrim(std::string &s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))));
+        [](unsigned char c) { return !std::isspace(c); }));
     return s;
 }
 
 // trim from end
 inline std::string &Grammar::rtrim(std::string &s) {
     s.erase(std::find_if(s.rbegin(), s.rend(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+        [](unsigned char c) { return !std::isspace(c); }).base(), s.end());
     return s;
 }
 
